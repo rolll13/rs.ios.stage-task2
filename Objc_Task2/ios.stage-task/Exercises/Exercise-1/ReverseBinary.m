6fa22c23152d9cf407cdf9cb0376b9cd2269dc57
rolll13 <r.mikulich@mail.ru>
2021-05-14 16:32:46 +0300

    feat:add exercise1 and exercise3 implementtation

--- Objc_Task2/ios.stage-task/Exercises/Exercise-1/ReverseBinary.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-1/ReverseBinary.m
@@ -1,5 +1,12 @@
 #import "ReverseBinary.h"
 
 UInt8 ReverseInteger(UInt8 n) {
-    return 0;
+    int number = 0;
+    for (int i = 0; i<8; i++) {
+        if (n%2) {
+            number+=pow(2, 7-i);
+        }
+        n/=2;
+    }
+    return number;
 }
--- Objc_Task2/ios.stage-task/Exercises/Exercise-3/IPFormatConverter.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-3/IPFormatConverter.m
@@ -3,7 +3,33 @@
 @implementation IPFormatConverter
 
 - (NSString *)ipFormatConverter:(NSArray *)numbersArray {
-    return nil;
+    NSString *negativeNumber = @"Negative numbers are not valid for input.";
+    NSString *numberOutOfRange = @"The numbers in the input array can be in the range from 0 to 255.";
+    
+    if (numbersArray.count == 0) {
+            return @"";
+        }
+    
+    for (NSNumber *number in numbersArray){
+        if ([number intValue]<0) {
+            return negativeNumber;
+            
+        }
+        if ([number intValue]>255) {
+            return numberOutOfRange;
+        }
+    }
+    int tempArray[4];
+    for (int i = 0; i<4; i++){
+        if (i<numbersArray.count){
+            tempArray[i] = [numbersArray[i] intValue];
+        } else {
+            tempArray[i] = 0;
+        }
+    }
+    NSString *output = [NSString stringWithFormat:@"%d.%d.%d.%d", tempArray[0], tempArray[1],tempArray[2], tempArray[3]];
+    
+    return output;
 }
 
 @end
